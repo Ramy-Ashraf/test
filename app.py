c6ebf2e42ee037d06dcc207676bb225002bdc6a3
Ramy-Ashraf <ramy.abdelmaseeh01@eng-st.cu.edu.eg>
2022-03-06 18:05:39 +0200

    hello world

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+
+# TODO: create new Flask app
+app = Flask
+
+
+@app.route("/")
+def main_page():
+    # TODO: return index.html
+    return render_template("index.html")
+
+
+# TODO: Add route for sign up
+@app.route('/sign_up')
+def sign_up():
+    # TODO: get user input from request
+    email = request
+    username = request
+    password = request
+
+    if not user_exists(email=email, username=username, password=password):
+        return "<h2>New user has been created</h2>"
+    else:
+        return "<h2>This user already exists</h2>"
+
+
+def user_exists(email, username, password):
+    # TODO: check for user if exists, you can use an array as your records.
+    if email == "ramy.ashraf2015@gmail.com" and username == "Ramy" and password == "test":
+
+        return True
---
+++ static/js/utils.js
@@ -0,0 +1,5 @@
+function alert_welcome() {
+	// TODO: show alert with welcoming message
+    alert("Welcome ya bro");
+	// HINT: check alert()
+}
